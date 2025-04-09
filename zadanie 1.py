91d48c07f6fe53c984cbd7b447cc5dbd789d7641
Wiktoria Armańska <armanska.wiktoria@gmail.com>
2025-04-09 10:23:37 +0200

    done

--- zadanie 1.py
+++ zadanie 1.py
@@ -1,4 +1,4 @@
-# Napisz, na przynajmniej cztery sposoby, funkcję zamieniającą listę liczb całkowitych na łańcuch odpowiadających im znaków unikodu. Przykładowo, lista [65, 66] powinna zostać zamieniona na AB.
+# Napisz, na przynajmniej cztery sposoby, funkcję zamieniającą listę liczb całkowitych w stringa odpowiadających im znaków unikodu. Przykładowo, lista [65, 66] powinna zostać zamieniona na AB.
 # Wykorzystaj decorator do testów wydajności
 
 from time import perf_counter
@@ -21,8 +21,33 @@ def execution_time(function):  #dekorator
 
 @execution_time
 def test_function_1(int_list: List[int]):
-
+    unicode_list: str = ""
+    for number in int_list:
+        unicode_list = unicode_list + chr(number) + " "
 
     return unicode_list
 
-test_function(5,10, c=4)
+@execution_time
+def test_function_2(int_list: List[int]):
+    chr_list = map(chr, int_list)
+    return " ".join(chr_list)
+
+@execution_time
+def test_function_3(int_list: List[int]):
+    chr_list = [chr(i) for i in int_list]
+    return " ".join(chr_list)
+
+@execution_time
+def test_function_4(int_list: List[int]):
+    unicode_list: str = ""
+    chr_list = []
+    for i in range(len(int_list)):
+        chr_list.append(chr(int_list[i]))
+    for word in chr_list:
+        unicode_list = unicode_list + word + " "
+    return unicode_list
+
+test_function_1([124,68,43,164,122])
+test_function_2([124,68,43,164,122])
+test_function_3([124,68,43,164,122])
+test_function_4([124,68,43,164,122])
